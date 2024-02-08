4baa3229ff9d9d0e3773794b5bc5323fb1407ada
whitesnow1022127 <snow1022127@gmail.com>
2024-02-08 17:46:22 +1000

    config

--- polls/models.py
+++ polls/models.py
@@ -14,4 +14,12 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+class Vote(models.Model):
+    choice = models.ForeignKey(Choice, related_name='votes', on_delete=models.CASCADE)
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    voted_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    class Meta:
+        unique_together = ("poll", "voted_by")
+        
+
 # Create your models here.

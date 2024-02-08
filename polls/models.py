3b29c7500eef7174c93edeb5e4802f8198b20da3
whitesnow1022127 <snow1022127@gmail.com>
2024-02-09 01:21:05 +1000

    API View

--- polls/models.py
+++ polls/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class Poll(models.Model):
     question = models.CharField(max_length=100)
-    create_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     pub_date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
@@ -20,6 +20,6 @@ class Vote(models.Model):
     voted_by = models.ForeignKey(User, on_delete=models.CASCADE)
     class Meta:
         unique_together = ("poll", "voted_by")
-        
+
 
 # Create your models here.
---
+++ polls/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import polls_detail,polls_list,PollList,PollDetail
+
+urlpatterns = [
+    path("polls/",PollList.as_view(), name="polls_list"),
+    path("polls/<int:pk>/",PollDetail.as_view(),name="polls_detail"),
+]
